f653d9c14d9aa2bd07e0a1bb190342364ba9bea8
MingLi <152678744+XingZiNina@users.noreply.github.com>
2026-03-22 23:01:09 +0800

    Delete src/main/java/com/fluxcraft/miaomenu directory

--- src/main/java/com/fluxcraft/miaomenu/menu/action/MenuAction.java
+++
@@ -1,8 +0,0 @@
-package com.fluxcraft.MiaoMenu.menu.action;
-
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-
-public interface MenuAction {
-    void execute(Player player, String content, Plugin plugin);
-}
