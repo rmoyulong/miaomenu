12ce87d05a1a128c8e04a7452349a600d2e0fc78
MingLi <152678744+XingZiNina@users.noreply.github.com>
2026-03-22 23:00:40 +0800

    Delete src/main/java/com/fluxcraft/MiaoMenu directory

--- src/main/java/com/fluxcraft/MiaoMenu/foliacall/FoliaAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.fluxcraft.MiaoMenu.foliacall;
-
-import org.bukkit.entity.Entity;
-import org.bukkit.plugin.Plugin;
-
-public interface FoliaAdapter {
-    void runTaskLaterForEntity(Plugin plugin, Entity entity, Runnable task, long delay);
-}
