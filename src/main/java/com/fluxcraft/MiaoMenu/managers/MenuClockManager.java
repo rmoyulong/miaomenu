aee7c2babaab12ff616e9fb5b29c0abd46be7eda
rmoyulong <mojialin99@qq.com>
2026-06-25 12:19:47 +0800

    v1.3.3 [Java 21编译]

--- src/main/java/com/fluxcraft/MiaoMenu/managers/MenuClockManager.java
+++ src/main/java/com/fluxcraft/MiaoMenu/managers/MenuClockManager.java
@@ -30,7 +30,7 @@ public class MenuClockManager {
             String rawName = Lang.get("menu.clock.name");
 			ArrayList<String> lore = new ArrayList<>();
 			lore.add("§e==================");
-			lore.add("§b=====小森监制=====");
+			lore.add("§b====小森监制=====");
 			lore.add("§e==================");
             Component nameComponent = Component.text(rawName)
                     .decoration(TextDecoration.ITALIC, false);
